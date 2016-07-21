df8bb2e30fb49bf3ab035eb7880507db7f6c8a8f
Ryan Hunt <ryan@ryanhunt.net>
2016-07-21 22:18:53 +1000

    secret check

--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 
 include 'secret.php';
 
+if(isset($_POST['secret']) && (trim($_POST['secret']) != '') ) {
+    $s=$_POST['secret'];
+}
+else {
+    echo "Nope.";
+    exit;
+}
+
 $file = '/tmp/ggggggg.txt';
 
 $fp = fopen($file, 'w');
 fwrite($fp, print_r($_POST, TRUE));
 fwrite($fp, print_r($_BODY, TRUE));
 fclose($fp);
+
+
+
 
 ?>
