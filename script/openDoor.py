74298121c425742e091e15b0cc1707867a948bdd
Ryan Hunt <ryan@MacPro.home>
2016-08-04 22:11:48 +1000

    added file temp writing

--- script/openDoor.py
+++ script/openDoor.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
-tempFile = '/tmp/GarageDoor.opened'
+tempFile = '/tmp/GarageDoor.status'
+# 1 = opening
+# 2 = opened
+# 3 = closing
+# 4 = closed
+
 
 import RPi.GPIO as GPIO
 
@@ -14,12 +19,18 @@ target = open(tempFile, 'w')
 target.truncate()
 
 if GPIO.input(18):
-        print "Already on, quitting."
+        print "Already on, closing..."
+        target.write("3\n")
+        GPIO.output(18,GPIO.LOW)
+        target.write("4\n")
 else:
         print "Turning on..."
+        target.write("1\n")
         GPIO.output(18,GPIO.HIGH)
-        target.write("1")
-        target.close()
+        target.truncate()
+        target.write("2\n")
+        
+target.close()
         
 
 # This resets all pins
